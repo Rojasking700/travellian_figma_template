f2f49299aa3a8fcde2b5fc0d2b21efaebc9d5401
Gabriel Rojas <grojaspiano@gmail.com>
2024-12-28 00:05:43 -0600

    cards pt1

--- components/LandingPage/Popular.jsx
+++ components/LandingPage/Popular.jsx
@@ -4,6 +4,31 @@ import BackgrounElement from '~/public/imgs/LandingPage/hero/HeroSection.png';
 import styles from '~/scss/LandingPage/hero.module.scss';
 
 export default function Popular() {
+
+  const destinations = [
+    {
+      title: "Monument of Berlin",
+      city: "Berlin, Germany",
+      img: "",
+    },
+    {
+      title: "Millennium Bridge",
+      city: "London, United Kingdom",
+      img: "",
+    },
+    {
+      title: "Rialto Bridge",
+      city: "Venice, Italy",
+      img: "",
+    },
+    {
+      title: "Sea of Orange Tiles",
+      city: "Lisbon, Portugal",
+      img: "",
+    },
+  ]
+
+
   return (
     <div className={styles.cardSection}>
       <div className={styles.cardSectionTop}>
@@ -11,8 +36,10 @@ export default function Popular() {
           <h3>Popular Destinations</h3>
           <div className={styles.cardSectionHeadingLine}></div>
           <p>Most popular destinations around the world, from historical places to natural wonders.</p>
-
         </div>
+      </div>
+      <div>
+
       </div>
     </div>
   )

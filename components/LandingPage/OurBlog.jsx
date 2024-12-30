bb99e60b399a99a9d61968d7cc49305bf67d9e6b
Rojasking700 <grojaspiano@gmail.com>
2024-12-29 19:46:35 -0600

    our blog

--- components/LandingPage/OurBlog.jsx
+++ components/LandingPage/OurBlog.jsx
@@ -1,11 +1,9 @@
 
 import Image from 'next/image';
-import Lisbon from '~/public/imgs/LandingPage/offer/Lisbon.png';
-import Athens from '~/public/imgs/LandingPage/offer/Athens.png';
-import Rome from '~/public/imgs/LandingPage/offer/Rome.png';
-import Rating from '~/public/imgs/LandingPage/offer/Rating.png';
+import BlogImage from '~/public/imgs/LandingPage/blog/BlogImage.png';
 import styles from '~/scss/LandingPage/hero.module.scss';
 import { Rubik  } from "next/font/google";
+import Link from 'next/link';
 
 const getRubik = Rubik({
   // variable: "--font-geist-mono",
@@ -15,37 +13,14 @@ const getRubik = Rubik({
 
 export default function OurBlog() {
 
-  const offers = [
-    {
-      city: 'Lisbon, Portugal',
-      stars: 5,
-      description: '5 nights and 4 days in 5 star hotel, breakfast and lunch included. Very popular during the renaissance. Passage and going through the cites of the world in classical literature.',
-      price: 500,
-      img: Lisbon,
-    },
-    {
-      city: 'Athens, Greece',
-      stars: 5,
-      description: '5 nights and 4 days in 5 star hotel, breakfast and lunch included. Very popular during the renaissance. Passage and going through the cites of the world in classical literature.',
-      price: 800,
-      img: Athens,
-    },
-    {
-      city: 'Rome, Italy',
-      stars: 5,
-      description: '5 nights and 4 days in 5 star hotel, breakfast and lunch included. Very popular during the renaissance. Passage and going through the cites of the world in classical literature.',
-      price: 750,
-      img: Rome,
-    },
-  ]
 
   return (
-    <div className={`${styles.cardSection} ${getRubik.className}`}>
+    <div className={`${styles.cardSection}`}>
       <div className={styles.cardSectionTop}>
         <div className={styles.cardSectionHeading}>
           <h3>Our Blog</h3>
           <div className={styles.cardSectionHeadingLine}></div>
-          <p>An insight the incredible experience in the world</p>
+          <p className={`${getRubik.className}`} >An insight the incredible experience in the world</p>
         </div>
         <div>
 
@@ -53,10 +28,21 @@ export default function OurBlog() {
       </div>
       <div className={styles.blogSection}>
         <div>
-          
+          <Image 
+            src={BlogImage}
+            alt='Blog Image'
+          />
         </div>
-        <div>
-
+        <div className={styles.blogSectionArticle} >
+          <h3>Beautiful Italy Let's travel</h3>
+          <p  className={`${getRubik.className}`}>
+            But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and 
+            I will give you a complete account of the system and expound the actual teachings of the great explorer of the truth, 
+            the master- builder of human happiness. No one rejects, dislike, or avoids plasure itself, because it is pleasure, 
+            but because those who do not know how to pursue pleasure rationally encounter consequences that are extremly painful. 
+            Nor again is there anyone who loves or pursues.
+          </p>
+          <Link href='#' className={`${getRubik.className}`} >Read More {'->'} </Link>
         </div>
 
       </div>
